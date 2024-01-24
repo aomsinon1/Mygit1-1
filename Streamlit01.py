1849df0ef3002c756690acfff87b91eb2de537e2
aomsinon1 <156872565+aomsinon1@users.noreply.github.com>
2024-01-24 10:48:26 +0700

    .

--- Streamlit01.py
+++ Streamlit01.py
@@ -9,12 +9,12 @@ col1, col2, col3 = st.columns(3)
 
 with col1
    st.header("Versicolor")
-   st.image("https://c.pxhere.com/photos/f2/77/iris_flower_purple-1401467.jpg!d")
+   st.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fhome.kapook.com%2Fview274378.html&psig=AOvVaw3XNa6g6_kGBfSoQPsis8HN&ust=1706153658534000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMDL7ceO9YMDFQAAAAAdAAAAABAD")
 
 with col2
    st.header("Verginica")
-   st.image("https://www.fs.usda.gov/wildflowers/beauty/iris/Blue_Flag/images/iris_virginica/iris_virginica_virginica_lg.jpg")
+   st.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fbonmarche.co.th%2Fstore%2Fbuilding-b%2F%25E0%25B8%258A%25E0%25B8%25B8%25E0%25B8%2594%25E0%25B8%25B2%25E0%25B8%2594%25E0%25B8%25AD%25E0%25B8%2581%25E0%25B9%2584%25E0%25B8%25A1%25E0%25B9%2589%25E0%25B8%25AA%25E0%25B8%25A7%25E0%25B8%25A2-chuda-beauty-flower%2F&psig=AOvVaw3XNa6g6_kGBfSoQPsis8HN&ust=1706153658534000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMDL7ceO9YMDFQAAAAAdAAAAABAH")
 
 with col3
    st.header("Setosa")
-   st.image("https://upload.wikimedia.org/wikipedia/commons/4/41/Iris_versicolor_3.jpg")
+   st.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F394698354835302534%2F&psig=AOvVaw3XNa6g6_kGBfSoQPsis8HN&ust=1706153658534000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMDL7ceO9YMDFQAAAAAdAAAAABAQ")
