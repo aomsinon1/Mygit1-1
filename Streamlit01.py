3ff63f048173d8ad86622a4fdafbfecdb1d5e3c2
aomsinon1 <156872565+aomsinon1@users.noreply.github.com>
2024-01-24 10:30:24 +0700

    .

--- Streamlit01.py
+++ Streamlit01.py
@@ -1,4 +1,20 @@
 import streamlit as st
+import pandas as st
 st.title("การทดสอบสร้างเว็บด้วยPython")
-st.image("12.jpg")
+st.image("data.jpeg")
 st.header("การนำเสนอข้อมูลกราฟด้วย Python")
+
+
+col1, col2, col3 = st.columns(3)
+
+with col1
+   st.header("Versicolor")
+   st.image("https://c.pxhere.com/photos/f2/77/iris_flower_purple-1401467.jpg!d")
+
+with col2
+   st.header("Verginica")
+   st.image("https://www.fs.usda.gov/wildflowers/beauty/iris/Blue_Flag/images/iris_virginica/iris_virginica_virginica_lg.jpg")
+
+with col3
+   st.header("Setosa")
+   st.image("https://upload.wikimedia.org/wikipedia/commons/4/41/Iris_versicolor_3.jpg")
