331e18bd734e37bdaf3a9a03976620f24f9d433b
aomsinon1 <156872565+aomsinon1@users.noreply.github.com>
2024-01-24 11:50:29 +0700

    Update Streamlit01.py

--- Streamlit01.py
+++ Streamlit01.py
@@ -61,3 +61,15 @@ ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
 st.pyplot(fig1)
 
 
+if show_pie_chart:
+    labels = 'sepal.width', 'sepal.length', 'petal.width', 'petal.length'
+    x1 = df['sepal.width'].mean()
+    x2 = df['sepal.length'].mean()
+    x3 = df['petal.width'].mean()
+    x4 = df['petal.length'].mean()
+    sizes = [x1, x2, x3, x4]
+    explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'sepal.length')
+
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+            shadow=True, startangle=90)
